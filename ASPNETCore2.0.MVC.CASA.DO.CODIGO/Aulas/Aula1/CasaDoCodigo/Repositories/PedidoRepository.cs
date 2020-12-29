04ad7698331c2255dbd9a3447364458b5dcdaf45
ralvescosta <rafael.rac.mg@gmail.com>
2020-12-29 07:50:14 -0300

    chore: organizado estrutura do projeto

--- ASPNETCore2.0.MVC.CASA.DO.CODIGO/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ ASPNETCore2.0.MVC.CASA.DO.CODIGO/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,10 +1,25 @@
 ﻿using CasaDoCodigo.Models;
-
+using Microsoft.AspNetCore.Http;
 
 namespace CasaDoCodigo.Repositories
 {
     public class PedidoRepository: BaseRepository<Pedido>, IPedidoRepository
     {
-        public PedidoRepository(ApplicationContext contexto) : base(contexto) { }
+        private readonly IHttpContextAccessor contextAccessor;
+        public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAccessor) : base(contexto) {
+            this.contextAccessor = contextAccessor;
+        }
+
+        private int? GetPedidoId()
+        {
+            return contextAccessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        private void SetPedidoId(int pedidoId)
+        {
+            contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
+        }
     }
+
+
 }
