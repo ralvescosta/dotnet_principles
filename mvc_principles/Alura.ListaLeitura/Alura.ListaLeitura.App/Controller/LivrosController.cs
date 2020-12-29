cd93e99562631115ea5d7b91e581d82e8d4fbfb2
ralvescosta <rafael.rac.mg@gmail.com>
2020-12-29 08:01:06 -0300

    chore: estrutura do projeto

---
+++ mvc_principles/Alura.ListaLeitura/Alura.ListaLeitura.App/Controller/LivrosController.cs
@@ -0,0 +1,56 @@
+﻿using Alura.ListaLeitura.App.Model;
+using Alura.ListaLeitura.App.Repositorio;
+using Alura.ListaLeitura.App.View;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class LivrosController : Controller
+    {
+        public IActionResult ParaLer()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.ParaLer.Livros;
+
+            return View("para-ler");
+        }
+
+        public IActionResult Lendo()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lendo.Livros;
+
+            return View("lendo");
+        }
+
+        public IActionResult Lidos()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lendo.Livros;
+
+            return View("lidos");
+        }
+
+
+        public string Detalhes(int id)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            Livro livro = null;
+            foreach (var item in _repo.Todos)
+            {
+                if (item.Id == id)
+                {
+                    livro = item;
+                    break;
+                }
+            }
+
+            if (livro == null)
+            {
+                return "";
+            }
+            return livro.Detalhes();
+        }
+    }
+}
---
+++ mvc_principles/Alura.ListaLeitura/Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
+}
